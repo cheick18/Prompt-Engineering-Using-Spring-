5d8a81a4d46e0d258480442647d448b8e5222679
cheick18 <97468180+cheick18@users.noreply.github.com>
2024-07-20 22:58:03 +0100

    Prompt App using AI

---
+++ README.md
@@ -0,0 +1,19 @@
+# Prompt Engineering using Spring AI
+Application basée sur Spring utilisant Prompt Engineering, permettant aux utilisateurs de poser des questions sur l'analyse des sentiments et de recevoir instantanément des réponses précises.
+
+
+## Code utilisant ZeroPrompt pour l'analyse du sentiment
+
+![Code utilisant ZeroPrompt pour l'analyse du sentiment](capture/code_zero_short_prompt.png)
+
+## Code utilisant du Few-Shot Prompt pour analyser le sentiment
+![Code utilisant ZeroPrompt pour l'analyse du sentiment](capture/code_few_short_code.png)
+
+## Resultat obtenu avec les deux types de prompt
+![Code utilisant ZeroPrompt pour l'analyse du sentiment](capture/resultat_zero_short_prompt.png)
+
+## Code utiliser pour le RestController
+![Code utiliser pour le RestController](capture/rest.png)
+
+## Exemple de resultat du prompt en utilisant le restcontroller
+![Code utilisant ZeroPrompt pour l'analyse du sentiment](capture/resultat_prompt_avec_rest_controller.png)
---
+++ src/main/java/org/sid/spring_ia_rag/Test.java
@@ -0,0 +1,62 @@
+package org.sid.spring_ia_rag;
+
+import org.springframework.ai.chat.messages.AssistantMessage;
+import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.ai.openai.OpenAiChatOptions;
+import org.springframework.ai.openai.api.OpenAiApi;
+
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args){
+        OpenAiApi openAiApi= new OpenAiApi("sk-proj-5YzkkX4AJK2IvmflecSaT3BlbkFJP6xIwByJsGILB64bfMOg");
+        OpenAiChatModel openAiChatModel= new OpenAiChatModel(openAiApi, OpenAiChatOptions.builder()
+                .withModel("gpt-4o")
+                .withTemperature(0F)
+                .withMaxTokens(300)
+                .build());
+        String systemMessageText= """ 
+Vous êtes un assistant spécialisé dans le domaine de l'analyse des sentiments. 
+Votre tâche est d'extraire à partir d'un commentaire le sentiment sur différents aspects
+des ordinateurs achetés par les clients. Les aspects qui nous intéressent sont : l'écran, la souris et le clavier.
+Le sentiment peut etre: positive, negative ou neutre.
+Le résultat attendu sera au format JSON avec les champs suivants :
+  - clavier : le sentiment relatif au clavier
+  - souris : le sentiment relatif à la souris
+  - écran : le sentiment relatif à l'écran
+                """;
+        String userInputText= """
+                Je ne suis satisfait par laqualité de l'écran, mais le clavier est mauvais alors que la souris est plutot de bonne qualité.
+                par ailleur je pense que cet ordinateur consomme beaucoup d'ànergie
+                """;
+
+        String userInputText1= """
+                Je suis satisfait par laqualité de l'écran, mais le clavier est mauvais alors que la souris la qualité est plutot moyenne.
+                par ailleur je pense que cet ordinateur consomme beaucoup d'ànergie
+                """;
+        UserMessage userMessage1=new UserMessage(userInputText);
+
+        String response1= """
+                {
+                "clavier":"negative",
+                "souris":"neutre",
+                "écran":"positive"
+                }
+                """;
+        AssistantMessage assistantMessage1= new AssistantMessage(response1);
+
+        SystemMessage systemMessage= new SystemMessage(systemMessageText);
+        UserMessage userMessage=new UserMessage(userInputText);
+        Prompt prompt=new Prompt(List.of(assistantMessage1,userMessage));
+        ChatResponse chatResponse= openAiChatModel.call(prompt);
+        System.out.println(chatResponse.getResult().getOutput().getContent());
+
+
+
+
+    }
+}
